1d6ca0f2a8c6f21325e3f58f3a160830a4b4069c
Andy Bryant <andybryant@FDOS-ABRYANT-1.local>
2020-03-06 10:14:04 -0500

    finished heredity

--- project2/heredity/heredity.py
+++ project2/heredity/heredity.py
@@ -116,7 +116,6 @@ def load_data(filename):
             }
     return data
 
-
 def powerset(s):
     """
     Return a list of all possible subsets of set s.
@@ -139,7 +138,9 @@ def joint_probability(people, one_gene, two_genes, have_trait):
         * everyone in set `have_trait` has the trait, and
         * everyone not in set` have_trait` does not have the trait.
     """
+    # Initialize people_prob dict for use throughout
     people_prob = {}
+    # Loop through all people
     for person in people:
         people_prob[person] = None
         # Get the number of copies of the gene
@@ -148,56 +149,39 @@ def joint_probability(people, one_gene, two_genes, have_trait):
         has_trait = check_has_trait(person, have_trait)
         # Get the prob of trait given the number of copies and if has trait
         has_trait_prob = PROBS["trait"][person_num_copies][has_trait]
-
+        # Get parent information
         mother = people[person]['mother']
         father = people[person]['father']
-        naive_prob = PROBS['gene'][person_num_copies]
-
         # If no parent information
         if mother is None and father is None:
-            has_gene_prob = naive_prob
+            has_gene_prob = PROBS['gene'][person_num_copies]
         else:
+            # Get the number of gene copies for parents
             mother_num_copies = get_num_copies(mother, one_gene, two_genes)
             father_num_copies = get_num_copies(father, one_gene, two_genes)
-
+            # Calculate prob of inheritance by each parent
             mother_gives_gene = get_prob_parent_gives_gene(mother_num_copies)
             father_gives_gene = get_prob_parent_gives_gene(father_num_copies)
-
-            # Working on it...
-
+            # Calculate inheritance prob, based on number of copies person has
             if person_num_copies == 0:
-                has_gene_prob = mother_gives_gene + father_gives_gene
+                # If 0, multiply both exclusive possibilities
+                has_gene_prob = (mother_gives_gene + (1 - father_gives_gene)) * ((1 - mother_gives_gene) + father_gives_gene)
             elif person_num_copies == 1:
+                # If 1, add both exclusive  possibilities
                 has_gene_prob = (mother_gives_gene * (1 - father_gives_gene)) + ((1 - mother_gives_gene) * father_gives_gene)
-            elif person_num_copies == 2:
-                has_gene_prob = mother_gives_gene * father_gives_gene
             else:
-                raise ValueError('You messed up')
-                # mother_inherit_prob = mother_gives_gene + PROBS["mutation"]
-            # else:
-                # mother_inherit_prob =
-            # father_inherit_prob = ((1 - mother_gives_gene) * father_gives_gene)
-            # inherit_prob = mother_inherit_prob + father_inherit_prob
-
-            # if father_num_copies == 2:
-                # father_inherit_prob = ((1 - mother_gives_gene) * father_gives_gene)
-
-            # inherit_prob = mother_inherit_prob + father_inherit_prob
-            # Adding naive prob makes verified test fail
-
-            #  add different conditions for
-
-
-            # Closer for everyone overall
-            # inherit_prob = mother_gives_gene + father_gives_gene
-            # person_prob = PROBS['gene'][person_num_copies]
-            # has_gene_prob = inherit_prob + naive_prob
-
+                # If 2, multiply inclusive possibilities (they can both pass one gene)
+                has_gene_prob = mother_gives_gene * father_gives_gene
+        # Multiply by prob that has trait and store in people_prob dict
         people_prob[person] = has_gene_prob * has_trait_prob
-        # Multiply all values together
+    # multiply all values in people_prob dict and return (this is the joint probability)
     return numpy.prod(list(people_prob.values()))
 
 def get_prob_parent_gives_gene(num_copies):
+    '''
+    This helper function takes in the number of gene copies from a parent
+    and returns the probability that they will pass it on to their child.
+    '''
     if num_copies == 0:
         return PROBS["mutation"]
     elif num_copies == 1:
@@ -205,9 +189,13 @@ def get_prob_parent_gives_gene(num_copies):
     elif num_copies == 2:
         return 1 - PROBS["mutation"]
     else:
-        raise ValueError
+        raise ValueError('Number of copies must be 0, 1, or 2.')
 
 def get_num_copies(person, one_gene, two_genes):
+    '''
+    This helper function determines if person has one, two, or zero genes,
+    based on sets provided.
+    '''
     if person in one_gene:
         return 1
     elif person in two_genes:
@@ -216,6 +204,9 @@ def get_num_copies(person, one_gene, two_genes):
         return 0
 
 def check_has_trait(person, have_trait):
+    '''
+    This helper function checks if person is in have_trait set.
+    '''
     if person in have_trait:
         return True
     else:
@@ -228,10 +219,12 @@ def update(probabilities, one_gene, two_genes, have_trait, p):
     Which value for each distribution is updated depends on whether
     the person is in `have_gene` and `have_trait`, respectively.
     """
+    # Loop through probabilities
     for person in probabilities:
+        # Get number of copies in this iteration and add p to that part of the probabilities dict
         num_copies = get_num_copies(person, one_gene, two_genes)
         probabilities[person]["gene"][num_copies] += p
-
+        # Get has_trait this iteration and add p to that part of the probabilities dict
         has_trait = check_has_trait(person, have_trait)
         probabilities[person]["trait"][has_trait] += p
 
@@ -241,14 +234,18 @@ def normalize(probabilities):
     is normalized (i.e., sums to 1, with relative proportions the same).
     """
     keys = ['gene', 'trait']
+    # Loop through probabilities and access the keys in each person dict
     for person in probabilities:
         for key in keys:
             all_key_probs = []
+            # Get all values and add to new list
             for val in probabilities[person][key].values():
                 all_key_probs.append(val)
+            # Add all values together
             sum_all_key_probs = sum(all_key_probs)
 
             for k in probabilities[person][key].keys():
+                # Loop through all probability values again, normalizing with sum of all value probs
                 probabilities[person][key][k] = probabilities[person][key][k] / sum_all_key_probs
 
 if __name__ == "__main__":
--- project2/heredity/testHeredityMethods.py
+++ project2/heredity/testHeredityMethods.py
@@ -17,23 +17,41 @@ class TestHeredityMethods(unittest.TestCase):
             'Ron': {'name': 'Ron', 'mother': 'Molly', 'father': 'Arthur', 'trait': None}
         }
         self.people_2 = {
-            'Arthur': {'name': 'Arthur', 'mother': None, 'father': None, 'trait': False},
+            'Arthur': {'name': 'Arthur', 'mother': None, 'father': None, 'trait': True},
             'Hermione': {'name': 'Hermione', 'mother': None, 'father': None, 'trait': False},
-            'Molly': {'name': 'Molly', 'mother': None, 'father': None, 'trait': None},
+            'Molly': {'name': 'Molly', 'mother': None, 'father': None, 'trait': False},
             'Ron': {'name': 'Ron', 'mother': 'Molly', 'father': 'Arthur', 'trait': False},
             'Rose': {'name': 'Rose', 'mother': 'Ron', 'father': 'Hermione', 'trait': True}
         }
 
         self.probabilities_0 = {
-            'Harry': {'gene': {2: 0.1, 1: 0.3, 0: 0.1}, 'trait': {True: 0, False: 0}},
-            'James': {'gene': {2: 0.2, 1: 0.3, 0: 0}, 'trait': {True: 0, False: 0}},
-            'Lily': {'gene': {2: 0.04, 1: 0.01, 0: 0.01}, 'trait': {True: 0, False: 0}}
+            'Harry': {'gene': {2: 0.1, 1: 0.3, 0: 0.1}, 'trait': {True: 0.9, False: 0}},
+            'James': {'gene': {2: 0.2, 1: 0.3, 0: 0}, 'trait': {True: 0.8, False: 0}},
+            'Lily': {'gene': {2: 0.04, 1: 0.01, 0: 0.01}, 'trait': {True: 0, False: 1}}
         }
 
         self.probabilities_0_norm = {
-            'Harry': {'gene': {2: 0.2, 1: 0.6, 0: 0.2}, 'trait': {True: 0, False: 0}},
-            'James': {'gene': {2: 0.4, 1: 0.6, 0: 0}, 'trait': {True: 0, False: 0}},
-            'Lily': {'gene': {2: 0.2, 1: 0.0007115724000000001, 0: 0}, 'trait': {True: 0, False: 0}}
+            'Harry': {'gene': {2: 0.2, 1: 0.6, 0: 0.2}, 'trait': {True: 1.0, False: 0}},
+            'James': {'gene': {2: 0.4, 1: 0.6, 0: 0}, 'trait': {True: 1.0, False: 0}},
+            'Lily': {'gene': {2: 0.6666666666666666, 1: 0.16666666666666666, 0: 0.16666666666666666}, 'trait': {True: 0.0, False: 1.0}}
+        }
+
+        self.probabilities_1 = {
+            'Arthur': {'gene': {2: 0.0010715694733938617, 1: 0.006092582611726968, 0: 0.026465216431249546}, 'trait': {True: 0, False: 0.03362936851637028}},
+            'Charlie': {'gene': {2: 9.660732549755268e-05, 1: 0.004521731224790018, 0: 0.02901102996608282}, 'trait': {True: 0, False: 0.03362936851637028}},
+            'Fred': {'gene': {2: 0.00046837620497237127, 1: 0.023468597147422585, 0: 0.009692395163975381}, 'trait': {True: 0.03362936851637028, False: 0}},
+            'Ginny': {'gene': {2: 0.00018626549455374455, 1: 0.006790126989805804, 0: 0.026652976032010882}, 'trait': {True: 0.004187262672044918, False: 0.029442105844325498}},
+            'Molly': {'gene': {2: 0.0010715694733938615, 1: 0.006092582611726968, 0: 0.026465216431249543}, 'trait': {True: 0, False: 0.03362936851637028}},
+            'Ron': {'gene': {2: 0.00018626549455374474, 1: 0.006790157042721004, 0: 0.02665294597909568}, 'trait': {True: 0.00418726267204492, False: 0.029442105844325498}}
+            }
+
+        self.probabilities_1_updated = {
+            'Arthur': {'gene': {2: 0.0010715694733938617, 1: 0.006093070971598968, 0: 0.026465216431249546}, 'trait': {True: 0, False: 0.03362985687624228}},
+            'Charlie': {'gene': {2: 9.660732549755268e-05, 1: 0.0045222195846620185, 0: 0.02901102996608282}, 'trait': {True: 0, False: 0.03362985687624228}},
+            'Fred': {'gene': {2: 0.00046837620497237127, 1: 0.023469085507294585, 0: 0.009692395163975381}, 'trait': {True: 0.03362985687624228, False: 0}},
+            'Ginny': {'gene': {2: 0.00018675385442574454, 1: 0.006790126989805804, 0: 0.026652976032010882}, 'trait': {True: 0.004187751031916918, False: 0.029442105844325498}},
+            'Molly': {'gene': {2: 0.0010715694733938615, 1: 0.006093070971598968, 0: 0.026465216431249543}, 'trait': {True: 0, False: 0.03362985687624228}},
+            'Ron': {'gene': {2: 0.00018626549455374474, 1: 0.006790645402593004, 0: 0.02665294597909568}, 'trait': {True: 0.0041877510319169205, False: 0.029442105844325498}}
         }
 
     def testJointProbability(self):
@@ -41,15 +59,19 @@ class TestHeredityMethods(unittest.TestCase):
         # Verified
         self.assertEqual(joint_prob_0, 0.0026643247488)
 
-        # joint_prob_1 = hr.joint_probability(self.people_1, {}, {}, {"Fred"})
-        # self.assertEqual(joint_prob_1, 0.00875731494359588)
+        joint_prob_1 = hr.joint_probability(self.people_1, {}, {}, {"Fred"})
+        self.assertEqual(joint_prob_1, 0.008764324299878399)
 
-        # joint_prob_2 = hr.joint_probability(self.people_2, {}, {}, {"Rose"})
-        # self.assertEqual(joint_prob_2, 0.008495339559497134)
+        joint_prob_2 = hr.joint_probability(self.people_2, {}, {'Arthur'}, {'Arthur'})
+        self.assertEqual(joint_prob_2, 0.00022787243179683852)
 
-    # def testNormalize(self):
-    #     hr.normalize(self.probabilities_0)
-    #     self.assertEqual(self.probabilities_0, self.probabilities_0_norm)
+    def testNormalize(self):
+        hr.normalize(self.probabilities_0)
+        self.assertEqual(self.probabilities_0, self.probabilities_0_norm)
+
+    def testUpdate(self):
+        hr.update(self.probabilities_1, {'Fred', 'Ron', 'Arthur', 'Charlie', 'Molly'}, {'Ginny'},{'Ginny', 'Fred', 'Ron'}, 4.883598720000002e-07)
+        self.assertEqual(self.probabilities_1, self.probabilities_1_updated)
 
 if __name__ == '__main__':
     unittest.main()
