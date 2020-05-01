b75ef7f6e3f700e2b7e4d578641b9db7665d31d7
Andy Bryant <andybryant@FDOS-ABRYANT-1.local>
2020-05-01 18:02:39 -0400

    added comments in parserRunner

--- project6/parser/parserRunner.py
+++ project6/parser/parserRunner.py
@@ -5,16 +5,20 @@ import os
 import sys
 import glob
 
-# Not intended as a true test case
-# This is a simple runner that shows all output immediately
-
 class ParserTest(unittest.TestCase):
+    '''
+    Runs the parser on each file in the sentences directory. This is not intended as a true test case,
+    but rather as a way to review parser performance with each text file quickly.
+    '''
     def test_parser(self):
         filepath = os.getcwd() + '/sentences/*.txt'
+        # Get all filenames and reverse list to run sequentially
         filenames = glob.glob(filepath)[::-1]
+        # Loop through filenames and add them to argv sequentially
         for filename in filenames:
             test_argv = ["parser.py", filename]
             with patch.object(sys, 'argv', test_argv):
+                # Run main and check output
                 main()
 
 if __name__ == '__main__':
